9c3379ec7760120907d399a04ea9b6efc39787d2
Amir Moeen Rahmani <MrSinaRJ@gmail.com>
2024-08-26 15:11:18 +0330

    fix: add randomUUID as user _id when saving data

--- src/v1/user/application/user.service.ts
+++ src/v1/user/application/user.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { User } from '../domain/user.entity';
 import { USER_REPOSITORY, UserRepository } from '../domain/user.repository';
 
@@ -9,7 +10,7 @@ export class UserService {
   ) {}
 
   async register(username: string, password: string): Promise<User> {
-    const user = new User(undefined, username, password);
+    const user = new User(randomUUID(), username, password);
     return this.userRepository.save(user);
   }
 }
